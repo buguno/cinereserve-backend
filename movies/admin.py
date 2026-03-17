5302f97a99ce6698b9e412b6a3f9041f2c7bc7b6
buguno <brunnodesouzabezerra@gmail.com>
2026-03-17 16:35:19 -0300

    feat: adding movie in admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from movies.models import Movie
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_date', 'is_active')
+    search_fields = ('title',)
