2dc67ec113233c66dd76797dac1cf26e38c9c1d1
buguno <brunnodesouzabezerra@gmail.com>
2026-03-17 17:35:43 -0300

    feat: adding seat, room and showtime in admin

--- showtimes/admin.py
+++ showtimes/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from showtimes.models import Room, Seat, Showtime
+
+
+class SeatInline(admin.TabularInline):
+    model = Seat
+    extra = 10
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    inlines = [SeatInline]
+
+
+@admin.register(Showtime)
+class ShowtimeAdmin(admin.ModelAdmin):
+    list_display = ('movie', 'room', 'start_time')
+    list_filter = ('movie', 'room', 'start_time')
+
+
+admin.site.register(Seat)  # Caso queira editar assentos individualmente
