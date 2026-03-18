ecc185e1f6dedceeb4d280bedb0023c293279574
buguno <brunnodesouzabezerra@gmail.com>
2026-03-17 22:27:41 -0300

    feat: adding docs urls

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,42 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+def api_root(request):
+    return JsonResponse({
+        'message': 'Welcome to CineReserve API',
+        'docs': ['/docs/', '/redoc/'],
+    })
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='CineReserve API',
+        default_version='v1',
+        description='CineReserve API Documentation',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('', api_root),
+    path(
+        'docs/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui',
+    ),
+    path(
+        'redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc',
+    ),
     path('admin/', admin.site.urls),
     path('api/movies/', include('movies.urls')),
     path('api/showtimes/', include('showtimes.urls')),
