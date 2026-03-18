3b3de71f489c83dce83c6c6266b44c6976905d99
buguno <brunnodesouzabezerra@gmail.com>
2026-03-18 09:47:36 -0300

    feat(movies): cache movie list responses

--- movies/api/views.py
+++ movies/api/views.py
@@ -1,4 +1,6 @@
+from django.core.cache import cache
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
 
 from movies.api.serializers import MovieSerializer
 from movies.models import Movie
@@ -7,3 +9,15 @@ from movies.models import Movie
 class MovieListView(ListAPIView):
     queryset = Movie.objects.filter(is_active=True).order_by('release_date')
     serializer_class = MovieSerializer
+
+    def list(self, request, *args, **kwargs):
+        cache_key = 'movies:list'
+        cached_data = cache.get(cache_key)
+
+        if cached_data is not None:
+            return Response(cached_data)
+
+        response = super().list(request, *args, **kwargs)
+        cache.set(cache_key, response.data, timeout=60)
+
+        return response
