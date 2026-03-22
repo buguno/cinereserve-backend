a8e9fb73e17588f900211c87ebd4ba6ab474fa41
buguno <brunnodesouzabezerra@gmail.com>
2026-03-22 18:32:07 -0300

    fix(testing): update pytest discovery and docs settings lookup

--- core/urls.py
+++ core/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-
-from core import settings
 
 
 def api_root(request):
